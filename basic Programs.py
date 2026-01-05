2a34e00e8d5545f0d49f62eeb4f2154320db1218
Samarth0325 <samarthdeshmukhe2@gmail.com>
2026-01-05 16:58:29 +0530

    Basic Python Programs Based On Variables.

---
+++ basic Programs.py
@@ -0,0 +1,29 @@
+#Checking the datatype of variables
+a = 10
+b = 5.5
+c = "Python"
+d = True
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+#programm of assigning a multiple values
+a, b, c = 10, 20, 30
+
+print("a =", a)
+print("b =", b)
+print("c =", c)
+
+#Program to calculate total and average
+m1 = float(input("Enter marks of subject 1: "))
+m2 = float(input("Enter marks of subject 2: "))
+m3 = float(input("Enter marks of subject 3: "))
+
+total = m1 + m2 + m3
+average = total / 3
+
+print("Total =", total)
+print("Average =", average)
+
