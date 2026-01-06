37f10c34fced47a38483b212130456d1fbc7d7ad
Samarth0325 <shreyashbabar91@gmail.com>
2026-01-06 15:15:35 +0530

    learn about declaration of dictinary with displaying information of dict.

---
+++ dict.py
@@ -0,0 +1,12 @@
+# Creating a dictionary for student details
+student = {
+    "Name": "Shreyash",
+    "Math": 85,
+    "Science": 90,
+    "AI": 95
+}
+
+# Printing all key-value pairs
+print("Student Details:")
+for key, value in student.items():
+    print(key, ":", value)
