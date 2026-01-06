77f5b22a35994173cf6f8d306b9ac2e2e2353fb7
Samarth0325 <shreyashbabar91@gmail.com>
2026-01-06 15:04:23 +0530

    this is another list program where i can learn about finding the larest or smallest num from list.

---
+++ list 1.py
@@ -0,0 +1,12 @@
+# Creating a list of numbers
+nums = [12, 45, 2, 18, 90, 33]
+
+# Finding the largest number in the list
+largest = max(nums)
+
+# Finding the smallest number in the list
+smallest = min(nums)
+
+# Printing the results
+print("Largest number:", largest)
+print("Smallest number:", smallest)
