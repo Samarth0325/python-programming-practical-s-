0cc370a657f7f0c9a15c1e4915c5caa911fd9cf6
Samarth0325 <samarthdeshmukhe2@gmail.com>
2026-01-06 15:44:10 +0530

    learning about creating and displaying array.

---
+++ array.py
@@ -0,0 +1,8 @@
+# Importing array module
+import array as arr
+
+# Creating an integer array
+numbers = arr.array('i', [10, 20, 30, 40, 50])
+
+# Printing the array
+print("Array elements:", numbers)
