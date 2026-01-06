87eae4644e12f92d2decc0fbc70c0c30b088ce32
Samarth0325 <samarthdeshmukhe2@gmail.com>
2026-01-06 15:45:12 +0530

    Learn about accessing the element of array .

---
+++ array1.py
@@ -0,0 +1,8 @@
+import array as arr
+
+# Creating an array
+numbers = arr.array('i', [5, 10, 15, 20, 25])
+
+# Accessing elements using index
+print("First element:", numbers[0])
+print("Last element:", numbers[-1])
