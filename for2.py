b590b5aebed55d33f8855fa756e26e31c49909f6
Samarth0325 <samarthdeshmukhe2@gmail.com>
2026-01-06 15:36:42 +0530

    Learn about finding the Sum of numbers using for loop.

---
+++ for2.py
@@ -0,0 +1,9 @@
+# Initializing sum variable
+total = 0
+
+# Looping from 1 to 10
+for i in range(1, 11):
+    total = total + i   # Adding each number to total
+
+# Printing the sum
+print("Sum of first 10 natural numbers:", total)
