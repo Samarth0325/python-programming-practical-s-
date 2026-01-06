c96ca337746c79e9072a56b013b68e964466a540
Samarth0325 <shreyashbabar91@gmail.com>
2026-01-06 15:12:12 +0530

    Learn abot finding a lenght and maximum num of tuple

---
+++ tuple1.py
@@ -0,0 +1,12 @@
+# Creating a tuple
+t = (5, 15, 25, 35, 45)
+
+# Finding length of tuple
+length = len(t)
+
+# Finding maximum value
+maximum = max(t)
+
+# Printing the results
+print("Length of tuple:", length)
+print("Maximum value:", maximum)
