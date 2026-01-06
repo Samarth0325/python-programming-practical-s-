e7f06bcd3d6f3841bf8023c355e0d9960fb1c6fc
Samarth0325 <shreyashbabar91@gmail.com>
2026-01-06 15:19:33 +0530

    Learn about using value method finding the sum of marks give in the dict.

---
+++ dict1.py
@@ -0,0 +1,12 @@
+# Creating a dictionary of subjects and marks
+marks = {
+    "Math": 80,
+    "Physics": 70,
+    "Chemistry": 90
+}
+
+# Calculating total marks
+total = sum(marks.values())
+
+# Displaying the total
+print("Total Marks:", total)
