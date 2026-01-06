851add0565ffbebacf13cff2d8e80f3ea19c67e8
Samarth0325 <shreyashbabar91@gmail.com>
2026-01-06 15:23:01 +0530

    Learn about declaration of sets and finding the union and intersection of sets.

---
+++ set.py
@@ -0,0 +1,13 @@
+# Creating two sets
+A = {1, 2, 3, 4}
+B = {3, 4, 5, 6}
+
+# Finding union of sets
+union_set = A.union(B)
+
+# Finding intersection of sets
+intersection_set = A.intersection(B)
+
+# Printing results
+print("Union:", union_set)
+print("Intersection:", intersection_set)
