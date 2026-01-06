a1cd9b8eaf01e3996ba542d8dc232da9113c5433
Samarth0325 <shreyashbabar91@gmail.com>
2026-01-06 15:00:52 +0530

    list program where i was define a list and identifying the sum, average of a present elements in a list.

---
+++ list.py
@@ -0,0 +1,13 @@
+# Creating a list of numbers
+numbers = [10, 20, 30, 40, 50]
+
+# Calculating the sum of all elements
+total = sum(numbers)
+
+# Calculating the average
+average = total / len(numbers)
+
+# Displaying the results
+print("List:", numbers)
+print("Sum:", total)
+print("Average:", average)
