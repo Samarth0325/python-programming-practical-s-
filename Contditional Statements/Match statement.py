5e129ffad070ea2313ee23b1f7a75c2b4c0eb798
Samarth0325 <samarthdeshmukhe2@gmail.com>
2026-01-18 17:05:50 +0530

    Learn about match expression in python.

---
+++ Contditional Statements/Match statement.py
@@ -0,0 +1,25 @@
+#Variable Declaration 
+
+day = 5
+
+#initialization of match expression
+
+match day :
+    case "1":
+      print("Monday")
+    case "2":
+      print("Tuesday")
+    case "3":
+      print("Wednesday")
+    case "4":
+      print("Thursday")  
+    case "6":
+      print("Friday")
+
+      #we use if condition in match expression
+      if match==0:
+        print("Not found")
+
+#To combine all the cases use | pipe character as given by for understanding
+
+       #case "1|2|3|4|5":
