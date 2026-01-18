1e5b44fbeaf3c3dda62c198e191b53d58e981ecc
Samarth0325 <samarthdeshmukhe2@gmail.com>
2026-01-18 17:16:41 +0530

    Learn about how to use break statement in python to print no as we want.

---
+++ Contditional Statements/Break.py
@@ -0,0 +1,14 @@
+#variable declaration
+
+i = 7
+
+#Declaration of while loop:
+ 
+while i>6:
+    print(i)
+    i+=1
+
+    #initialization of break statement
+    if i==1000:
+        break
+    print(i)
