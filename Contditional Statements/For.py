16dd4703b3aed2687ba9c3b6929db9d0d45e91d4
Samarth0325 <samarthdeshmukhe2@gmail.com>
2026-01-18 17:30:39 +0530

    Learn about how to use for loop to access the element of list also learn about how to use break statement to stop the upto iteration we want.

---
+++ Contditional Statements/For.py
@@ -0,0 +1,11 @@
+#list declaration as a variable 
+
+fruits = ["apple","banana","cherry","mango"]
+
+#For loop initialization for accessing list element
+
+for i in fruits:
+    print(i)
+    if i == "banana":
+        break
+    print(i)
