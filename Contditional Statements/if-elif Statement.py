9aeefd4291c1bcf6d27a88597d63182695e6d0cf
Samarth0325 <samarthdeshmukhe2@gmail.com>
2026-01-18 16:21:04 +0530

    Learnig about If-elif statemnt in python.

---
+++ Contditional Statements/if-elif Statement.py
@@ -0,0 +1,12 @@
+#Declaration of variable
+a = 5
+
+#Declaration and initialization of if-elif statement.
+if a == 0:
+    print(False)
+
+#elif statement have a condition because if we want to declare many contion in pyton we use elif statement.    
+elif a == 4:
+       print(True)
+elif a != 0 and a==5:
+      print("yes a is not equal to 0 but equal to 5.")
