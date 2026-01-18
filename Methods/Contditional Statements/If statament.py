b3dd0338a94d7690591a035e2591ce2426cbd6fa
Samarth0325 <samarthdeshmukhe2@gmail.com>
2026-01-18 16:04:25 +0530

    Learning about declaration and initialization of if statement.

---
+++ Methods/Contditional Statements/If statament.py
@@ -0,0 +1,14 @@
+#Declaration of Variable
+x = 5
+
+#Declaration and Initialization If Statement
+if x>0:
+   print(True)
+   if x == 5:
+      print("Yes")
+      if x != 0:
+         print(True)
+         if x >= 5:
+            print("It is not greater then but equal to 5")
+            if x <= 5:
+               print("It is not less than but equal to 5")
