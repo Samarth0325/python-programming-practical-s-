da1befe00ebd5822a856b79b60ea05c9708bf90c
Samarth0325 <samarthdeshmukhe2@gmail.com>
2026-01-18 16:29:44 +0530

    Learn about how to use if-elif-else statement in python.

---
+++ Contditional Statements/If-elif-else statement.py
@@ -0,0 +1,14 @@
+#Declaration of variables
+
+a=5
+b=5
+c=10
+
+#Initilization of if-elif-else statement
+
+if a is b or b is c :
+  print(True)
+elif b is a and a is not b :
+  print(True)
+else:
+  print(False)
