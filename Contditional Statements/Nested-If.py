e6bf6da15fd1f45d8402c7ecb2c4083daa539f42
Samarth0325 <samarthdeshmukhe2@gmail.com>
2026-01-18 16:45:53 +0530

    Learn about Nested-if statement in python.

---
+++ Contditional Statements/Nested-If.py
@@ -0,0 +1,16 @@
+#if in python their is if statement is declared inside the if atatement is called nested-if.
+
+#declaration of variables
+
+#learning about how to use semicolon(;)
+a=5;b=5;c=10
+
+#initialization of Nested-if function
+
+if a<b and b>c:
+    print(True)
+    
+    if a==b and b==c:
+        print(True)
+    else:
+        print("Both the conditions are false")
