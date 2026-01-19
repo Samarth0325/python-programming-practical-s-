8fe133b868d23188f9cc9d0d8ba1433304380a79
Samarth0325 <samarthdeshmukhe2@gmail.com>
2026-01-19 14:08:19 +0530

    learn about how to define and return a function in python.

---
+++ Python Functions/fun1.py
@@ -0,0 +1,5 @@
+#Function is block of code which only runs when it is called.
+
+def my_function(): #Function defining 
+    print("hello, from a function") #Block of code which we want to return
+my_function()   #Functin calling
