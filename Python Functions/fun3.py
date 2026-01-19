af049fdab781ad23f8b51362fe523bea85dc7a14
Samarth0325 <samarthdeshmukhe2@gmail.com>
2026-01-19 14:41:27 +0530

    Learn about how to define an argument with function and how to call argument when we return an function.

---
+++ Python Functions/fun3.py
@@ -0,0 +1,6 @@
+#In this part we learn about how to define an argument with when we define a function in python.
+
+def my_function(name): #Defining a argument inside the parenthesis after defining a function.
+    print(name+"Deshmuke")   #return a output as Argument+Deshmukhe
+my_function("Samarth")
+my_function("parth")    #function calling
