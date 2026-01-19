dcf0b2837b3fc0d75e9507e4031f192d8a25d8ac
Samarth0325 <samarthdeshmukhe2@gmail.com>
2026-01-19 15:32:14 +0530

    Learn about how to return return different datatype using a python function.

---
+++ Python Functions/fun9.py
@@ -0,0 +1,7 @@
+#learn about how to return different datatype like list,tuple,sets in python function.
+
+def my_function():
+    return["apple","banana","cherry"]   #using different datatype
+fruit=my_function()
+print(fruit[0])   #returning a list item using index value.
+print(fruit[1])   #returning a list item using index value.
