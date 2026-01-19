981887d51e925522e7ac963a3503928c63e0c8f0
Samarth0325 <samarthdeshmukhe2@gmail.com>
2026-01-19 15:26:21 +0530

    Learn about how to return values for a arguments in function.

---
+++ Python Functions/fun8.py
@@ -0,0 +1,6 @@
+#Learn about how to return values for a arguments in a function.
+
+def my_function(x,y):
+    return x+y
+result=my_function (x=5,y=6)   #return a function with declaring a values.
+print(result)
