da5428e9eeb27e1091f78093daccf060ba4da477
Samarth0325 <samarthdeshmukhe2@gmail.com>
2026-01-19 15:42:25 +0530

    Learn about how to declare many *argument to define more than one argument.

---
+++ Arbitory Arguments/args.py
@@ -0,0 +1,6 @@
+#if in python function,we want to return many arguments we define arguments in form *argument inside the parenthesis
+
+
+def my_function(*kids): #we use *argument type to define many argument.
+    print("He is",kids)
+my_function("Samarth","parth","Shriram")  #we call many arguments when we call a function in this type of python function.
