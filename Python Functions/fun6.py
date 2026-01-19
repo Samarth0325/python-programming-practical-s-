9e2c3b958cdc780793d57df3320d423d9f69bb19
Samarth0325 <samarthdeshmukhe2@gmail.com>
2026-01-19 15:08:07 +0530

    Learn about how to call an argument in key:value form.

---
+++ Python Functions/fun6.py
@@ -0,0 +1,11 @@
+#learn about how to return an arguments in key value pair.
+
+def my_function(animal,name): #Function defining with arguments
+
+    print("I have a"," "+animal)   #animal argument calling
+
+
+    print("I have a"," "+animal+ " ", "Who's name is"," "+name)     #calling arguments systemstically using " ",+ operator for Space making.
+
+
+my_function(animal="dog",name="buddy")   #Argument calling using key:value.
