b161b46d7c1af8f23777f21044405b402103884f
Samarth0325 <samarthdeshmukhe2@gmail.com>
2026-01-19 15:20:18 +0530

    Learn about how to send any datatype as a argument in python function.

---
+++ Python Functions/fun7.py
@@ -0,0 +1,7 @@
+#learn about how to send any datatype as an argument in a function.
+
+def my_function(fruits):
+     for fruit in fruits:
+        print("fruit")
+my_fruit=["apple","banana","cherry"]
+my_function(my_fruit)
