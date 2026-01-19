2a8cf83e3387da823e888e7bee728a63ff909958
Samarth0325 <samarthdeshmukhe2@gmail.com>
2026-01-19 14:21:16 +0530

    Learn about how to return a function using variable and also got a knowledge why we use fun in python.

---
+++ Python Functions/fun2.py
@@ -0,0 +1,6 @@
+#in this programm we learn about why we use function in python.
+
+def fahrenheit_to_celcius(fahrenheit): #function defining using def keyword
+    return(fahrenheit-32)*5/9          #return statement
+x=fahrenheit_to_celcius(77)            #function calling using variable
+print(x)                               #returning a function using variable
