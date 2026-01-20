c6dc2f78af5f7dbf79840247a8ece7c38f0f3173
Samarth0325 <samarthdeshmukhe2@gmail.com>
2026-01-20 13:48:09 +0530

    Learn about how to declare class and also create object for calling a class.

---
+++ Oops practice/class.py
@@ -0,0 +1,8 @@
+#Learn about how to declare class and call it using creating the object of class.
+
+class Myclass: #class deaclaration using class keyword.
+    x=5
+
+#object creation
+y=Myclass()    #calling class by creating object
+print(y.x)
