991e6aa6c751a13c7968762cf7689e2c5b26eb57
Samarth0325 <samarthdeshmukhe2gmail.com>
2026-01-21 15:47:18 +0530

    Learn about file handling.

---
+++ File Handling/Operations.py
@@ -0,0 +1,6 @@
+# In this program we learn about how to make file handling operations
+
+# Open the file in read mode using 'with' statement
+# This automatically closes the file after reading
+with open("D:\Samarth\Python Programms\python-programming-practical-s-\File Handling", "r") as f:
+    print(f.read())
